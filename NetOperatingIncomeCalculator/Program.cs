5e0d09fb2d0ee0fbec414d7bda294848f1a6e0dc
NTCG25 <nigelmar2002@gmail.com>
2023-02-07 05:11:32 -0600

    Add project files.

---
+++ NetOperatingIncomeCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NetOperatingIncomeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Operating Revenue?: ");
+
+            int operatingRevenue = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is your Operating Expense?: ");
+
+            int operatingExpense = Convert.ToInt32(Console.ReadLine());
+
+            int NetOperatingIncome = operatingRevenue - operatingExpense;
+
+            Console.Write("Your Net Operating Income is " + NetOperatingIncome);
+        }
+    }
+}
